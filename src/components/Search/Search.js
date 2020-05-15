a2459270cc7c490544b92e083ed6acbbfe25b457
Elrey Belmonti <elreybelmonti@gmail.com>
2020-05-15 11:03:21 -0700

    styled Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,25 +1,59 @@
 import React from "react";
+import styled from "styled-components";
 import { string, func, arrayOf } from "prop-types";
 import upperCaseName from "../../utils/upperCaseName";
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 25px;
+  background-color: dodgerblue;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 10px;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  height: 30px;
+  font-size: 16px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  padding: 10px 5px;
+  border: 2px solid;
+  font-size: 16px;
+`;
+
 export default function Search({ query, onChange, breeds, randomBreed }) {
   return (
-    <div>
-      <form>
-        <label forhtml="breeds">Select Breed</label>
+    <Wrapper>
+      <Form>
+        <Label forhtml="breeds">
+          Select a breed and add it to your favorites list
+        </Label>
 
-        <select id="breeds" onChange={onChange}>
+        <Select id="breeds" onChange={onChange}>
+          <option value="">Select a Breed</option>
           {breeds?.map((breed, index) => (
             <option value={breed} key={index}>
               {upperCaseName(breed)}
             </option>
           ))}
-        </select>
-      </form>
-      <button type="button" onClick={randomBreed}>
+        </Select>
+      </Form>
+      <Button type="button" onClick={randomBreed}>
         Pick Random Breed
-      </button>
-    </div>
+      </Button>
+    </Wrapper>
   );
 }
 
