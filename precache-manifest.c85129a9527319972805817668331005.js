a86db6184a4d8bc19373e45b17a645da70e56d52
Elrey Belmonti <elreybelmonti@gmail.com>
2020-05-15 12:44:32 -0700

    Updates

---
+++ precache-manifest.c85129a9527319972805817668331005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdf485c454b0914bff9549074759f90",
+    "url": "/favorite-dog-breed/index.html"
+  },
+  {
+    "revision": "785aabd1716f6011eef3",
+    "url": "/favorite-dog-breed/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "047cdcd373824356249e",
+    "url": "/favorite-dog-breed/static/js/2.3430ad38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/favorite-dog-breed/static/js/2.3430ad38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785aabd1716f6011eef3",
+    "url": "/favorite-dog-breed/static/js/main.1d515f74.chunk.js"
+  },
+  {
+    "revision": "12a9f231e2c5369865e3",
+    "url": "/favorite-dog-breed/static/js/runtime-main.ad809946.js"
+  }
+]);
